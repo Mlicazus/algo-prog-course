c396ded79d3c3095b324353a9d98ec18d2c3c526
Волков Борис <volkovboris1501@gmail.com>
2026-09-10 18:24:57 +0300

    Lab01 task 2: temperature converter

---
+++ TempConverter/Program.cs
@@ -0,0 +1,6 @@
+﻿double celsius = 15.1;
+double fahrenheit = celsius * (9 / 5) + 32;
+const double difference = 273.15;
+double kelvin = celsius + difference;
+System.Console.WriteLine($"{celsius}°C = {fahrenheit}°F = {kelvin}K");
+
